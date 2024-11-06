2a2507ea03430c407251a54b631b18adc178c49c
Alec Christensen <alec_christensen@hotmail.com>
2024-11-06 16:22:18 +0100

    Skapat ProductController med GET endpoint för produkter

---
+++ ProductInfoService.API/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductInfoService.Business.Interfaces;
+using ProductInfoService.Models;
+
+namespace ProductInfoService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProductById(int id)
+    {
+        var product = _productService.GetProductById(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+}
+
