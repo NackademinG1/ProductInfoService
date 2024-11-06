0508fc270474633ad00f094fe07744383c1037aa
Alec Christensen <alec_christensen@hotmail.com>
2024-11-06 16:19:17 +0100

    Registrerat tjänster i Program.cs för beroendeinjektion

--- ProductInfoService.API/Program.cs
+++ ProductInfoService.API/Program.cs
@@ -1,19 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProductInfoService.Business.Interfaces;
+using ProductInfoService.Business.Services;
+using ProductInfoService.Data.Interfaces;
+using ProductInfoService.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Registrera tjänsterna
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
