5d7b219c3aef0b42f9d30c65ae093c6fffac0c48
Alec Christensen <alec_christensen@hotmail.com>
2024-11-06 16:16:49 +0100

    Lagt till dummy-implementation av ProductRepository

---
+++ ProductInfoService.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using ProductInfoService.Data.Interfaces;
+using ProductInfoService.Models;
+using System.Collections.Generic;
+
+namespace ProductInfoService.Data.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    public Product GetProductById(int id)
+    {
+        // Returnerar dummy-data
+        return new Product
+        {
+            Id = id,
+            Price = 99.99m,
+            Description = "Dummy Product",
+            StockStatus = "In Stock",
+            Images = new List<string> { "dummy1.jpg", "dummy2.jpg" }
+        };
+    }
+}
+
